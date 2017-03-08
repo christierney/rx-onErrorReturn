27cde91de12e84226c538c787b626a6cb24038ac
Chris Tierney <chris.tierney@gmail.com>
2017-03-08 16:16:50 -0500

    Add `onErrorReturn` example.

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,13 @@
+import rx.*
+class Main {
+    static void main(String[] args) {
+        Observable.merge([
+            Observable.just("b"),
+            Observable.just("b"),
+            Observable.error(new RuntimeException()),
+            Observable.just("a") ])
+        .onErrorReturn({ e -> "err" })
+        .subscribe({ s -> println "emitted $s"}, { println "errored" }, { println "complete" })
+        Thread.sleep(1000);
+    }
+}
